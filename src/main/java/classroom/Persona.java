37a65d804abe049a2cef9c981263bafa038209f2
Danilo <danilogiraldo33@gmail.com>
2020-09-08 18:51:13 -0500

    y bb güiski

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,15 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+    Persona(){
+    	totalPersonas++;
     }
 
     Persona(long cedula, String nombre) {
@@ -31,6 +33,7 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula=1;
         totalPersonas++;
     }
     
